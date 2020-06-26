5ef9a8e7a79fda2e4d3884df201f42a01c20873a
hardiz6996 <hardiknandwani420@gmail.com>
2020-06-27 01:12:48 +0530

    added notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,32 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
+
+function createNotes(noteItem) {
+    return <Note 
+        key = {noteItem.key}
+        number = {noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+    />;
+}
+
+
 function App() {
-    return <div>
-    <Header />
-    <Note />
-    <Footer />
-    </div>
+    return (<div>   
+    <Header />   
+        {notes.map( (noteItem) => 
+             <Note
+                key={noteItem.key}
+                number={noteItem.key}
+                title={noteItem.title}
+                content={noteItem.content}
+            />
+        )}
+    <Footer />   
+    </div>   
+    )
 }
 
 export default App;
